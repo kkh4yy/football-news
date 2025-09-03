c0ff8e968e5f3b9abc35caefbf7c568b6bc9facb
Khayra <khayra.tazkiya@ui.ac.id>
2025-09-03 15:18:16 +0700

    Complete tutorial 1: Django MVT implementation

--- football_news/settings.py
+++ football_news/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'main',
+    
+]   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
