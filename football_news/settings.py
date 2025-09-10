49a50248159d4b6a6d2c3b2ae1679087ebc16ce3
Khayra <khayra.tazkiya@ui.ac.id>
2025-09-10 18:09:35 +0700

    revisi

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "khayra-tazkiya-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://khayra-tazkiya-footballnews.pbp.cs.ui.ac.id/"
+    "https://khayra-tazkiya-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
